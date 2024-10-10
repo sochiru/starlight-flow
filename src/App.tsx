5d7404ae21390f5da103e2970a0c60b65d0c679b
Jan Duguran <janduguran@gmail.com>
2024-10-10 17:10:58 +0800

    Initial codebase

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import SignIn from './SignIn'
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <SignIn />
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+import { ThemeProvider } from '@/components/theme-provider'
+
+export default App
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
